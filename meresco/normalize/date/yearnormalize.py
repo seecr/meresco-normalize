b8f1356814e3e705e3384556511a7609befbd11e
Thijs Janssen <thijs@seecr.nl>
2013-03-06 11:36:42 +0100

    JJ/TJ: test for python2.6

--- meresco/normalize/date/yearnormalize.py
+++ meresco/normalize/date/yearnormalize.py
@@ -1,29 +1,31 @@
 ## begin license ##
 #
-#    Meresco Normalize is an open-source library containing normalization 
-#    components for use with Meresco
+# Meresco Normalize is an open-source library containing normalization
+# components for use with Meresco
 #
-#    Copyright (C) 2008-2009 Universiteit van Tilburg http://www.uvt.nl
-#    Copyright (C) 2008-2009 Technische Universiteit Delft http://www.tudelft.nl
-#    Copyright (C) 2008-2010 Seek You Too (CQ2) http://www.cq2.nl
+# Copyright (C) 2008-2010 Seek You Too (CQ2) http://www.cq2.nl
+# Copyright (C) 2008-2009 Technische Universiteit Delft http://www.tudelft.nl
+# Copyright (C) 2008-2009 Universiteit van Tilburg http://www.uvt.nl
+# Copyright (C) 2013 Seecr (Seek You Too B.V.) http://seecr.nl
 #
-#    This file is part of Meresco Normalize.
+# This file is part of "Meresco Normalize"
 #
-#    Meresco Normalize is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the License, or
-#    (at your option) any later version.
+# "Meresco Normalize" is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
-#    Meresco Normalize is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+# "Meresco Normalize" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with Meresco Normalize; if not, write to the Free Software
-#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# You should have received a copy of the GNU General Public License
+# along with "Meresco Normalize"; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 ## end license ##
+
 from re import compile
 
 class YearNormalize(object):
@@ -44,7 +46,7 @@ class YearNormalize(object):
         return None
 
     def unparsable(self, aString):
-        return self.normalize(aString) == None and aString or None
+        return aString if self.normalize(aString) is None else None
 
     def addRegex(self, aRegexString):
         self._yearRes.append(compile(aRegexString))
--- test/alltests.sh
+++ test/alltests.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 ## begin license ##
 #
-#    Meresco Normalize is an open-source library containing normalization 
-#    components for use with Meresco
-#    Copyright (C) 2010 Seek You Too (CQ2) http://www.cq2.nl
+# Meresco Normalize is an open-source library containing normalization
+# components for use with Meresco
 #
-#    This file is part of Meresco Normalize.
+# Copyright (C) 2010 Seek You Too (CQ2) http://www.cq2.nl
+# Copyright (C) 2013 Seecr (Seek You Too B.V.) http://seecr.nl
 #
-#    Meresco Normalize is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the License, or
-#    (at your option) any later version.
+# This file is part of "Meresco Normalize"
 #
-#    Meresco Normalize is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+# "Meresco Normalize" is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with Meresco Normalize; if not, write to the Free Software
-#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# "Meresco Normalize" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with "Meresco Normalize"; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 ## end license ##
 
 export PYTHONPATH=.
-python2.5 _alltests.py "$@"
+python2.6 _alltests.py "$@"
