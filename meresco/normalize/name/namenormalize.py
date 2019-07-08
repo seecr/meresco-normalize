d782db109c805bbb38a8c1123866cf0c936a2a8a
Thijs Janssen <thijs@seecr.nl>
2019-07-08 16:37:28 +0200

    TJ: refactoring to allow for familyname and givenname (no initials)

--- meresco/normalize/name/namenormalize.py
+++ meresco/normalize/name/namenormalize.py
@@ -1,30 +1,33 @@
 ## begin license ##
 #
-#    Meresco Normalize is an open-source library containing normalization 
-#    components for use with Meresco
+# Meresco Normalize is an open-source library containing normalization
+# components for use with Meresco
 #
-#    Copyright (C) 2008-2009 Universiteit van Tilburg http://www.uvt.nl
-#    Copyright (C) 2008-2009 Technische Universiteit Delft http://www.tudelft.nl
-#    Copyright (C) 2008-2010 Seek You Too (CQ2) http://www.cq2.nl
+# Copyright (C) 2008-2010 Seek You Too (CQ2) http://www.cq2.nl
+# Copyright (C) 2008-2009 Technische Universiteit Delft http://www.tudelft.nl
+# Copyright (C) 2008-2009 Universiteit van Tilburg http://www.uvt.nl
+# Copyright (C) 2019 Seecr (Seek You Too B.V.) https://seecr.nl
 #
-#    This file is part of Meresco Normalize.
+# This file is part of "Meresco Normalize"
 #
-#    Meresco Normalize is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the License, or
-#    (at your option) any later version.
+# "Meresco Normalize" is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
-#    Meresco Normalize is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+# "Meresco Normalize" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with Meresco Normalize; if not, write to the Free Software
-#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# You should have received a copy of the GNU General Public License
+# along with "Meresco Normalize"; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 ## end license ##
+
 from re import compile, UNICODE, IGNORECASE
+from collections import namedtuple
 
 def lower(initials):
     return initials.lower()
@@ -96,6 +99,8 @@ specificExpressions = [
 compiledExpressions = [(compile(expression, UNICODE),f) for expression,f in expressions]
 compiledSpecificExpressions = [compile(expression, flags|UNICODE) for flags, expression in specificExpressions]
 
+_BreakUpResult = namedtuple('BreakUpResult', ['lastname', 'givenname', 'initials', 'prefix'])
+
 def _breakUp(name):
     if type(name) != unicode:
         name = unicode(name)
@@ -103,7 +108,7 @@ def _breakUp(name):
         m = expression.match(name)
         if m:
             if 'result' in m.groupdict():
-                return m.groupdict()['result'].lower(), '', ''
+                return _BreakUpResult(m.groupdict()['result'].lower(), '', '', '')
             else:
                 return None
     for expression, initialsPostProcess in compiledExpressions:
@@ -111,12 +116,14 @@ def _breakUp(name):
         if m:
             lastname = m.groupdict()['lastname'].lower()
             lastname = lastname.strip()
+            givenname = m.groupdict().get('initials')
+            givenname = '' if givenname is None else givenname.lower().strip()
             if initialsPostProcess:
-                initials = addDots(initialsPostProcess(m.groupdict()['initials']))
+                initials = addDots(initialsPostProcess(givenname))
             else:
                 initials = ''
             prefix = m.groupdict().get('prefix', "")
-            return str(lastname), str(initials), str(prefix)
+            return _BreakUpResult(str(lastname), str(givenname), str(initials), str(prefix))
     return None
 
 
@@ -138,28 +145,28 @@ def _capitalizeSubstite(match):
 def _capitalize(aString):
     return str(_capitalizeRegexp.sub(_capitalizeSubstite, unicode(aString)))
 
-def _helsing_v((lastname, initials, prefix)):
-    prefix = ''.join([aChar == "'t" and aChar or aChar+"." for aChar in getInitials(prefix)])
-    result = _capitalize(lastname)
+def _helsing_v(parts):
+    prefix = ''.join([aChar == "'t" and aChar or aChar+"." for aChar in getInitials(parts.prefix)])
+    result = _capitalize(parts.lastname)
     if prefix:
         return result + ", " + prefix
     return result
 
-def _helsing_a_v((lastname, initials, prefix)):
-    prefix = ''.join([aChar == "'t" and aChar or aChar+"." for aChar in getInitials(prefix)])
-    initials = initials.split('.', 1)[0] + '.'
-    result = "%s, %s" % (_capitalize(lastname), _capitalize(initials))
+def _helsing_a_v(parts):
+    prefix = ''.join([aChar == "'t" and aChar or aChar+"." for aChar in getInitials(parts.prefix)])
+    initials = parts.initials.split('.', 1)[0] + '.'
+    result = "%s, %s" % (_capitalize(parts.lastname), _capitalize(initials))
     if prefix:
         return result + " " + prefix
     return result
 
-def _helsing_ab_van((lastname, initials, prefix)):
-    if initials:
-        result = "%s, %s" % (_capitalize(lastname), _capitalize(initials))
+def _helsing_ab_van(parts):
+    if parts.initials:
+        result = "%s, %s" % (_capitalize(parts.lastname), _capitalize(parts.initials))
     else:
-        result = _capitalize(lastname)
-    if prefix:
-        return result + " " + prefix
+        result = _capitalize(parts.lastname)
+    if parts.prefix:
+        return result + " " + parts.prefix
     return result
 
 def lastname(unparsedName):
@@ -180,11 +187,16 @@ def lastnameAndInitials(unparsedName):
 def firstLetter(unparsedName):
     parts = _breakUp(unparsedName)
     if parts:
-        lastname, initials, prefix = parts
-        lastname = unicode(lastname)
-        yield str(lastname[:1].upper())
+        yield str(unicode(parts.lastname)[:1].upper())
 
 def unparsable(unparsedName):
     parts = _breakUp(unparsedName)
     if not parts:
         yield unparsedName
+
+familyname = lastname
+def givenname(unparsedName):
+    parts = _breakUp(unparsedName)
+    if parts:
+        yield _capitalize(parts.givenname)
+
--- test/namenormalizetest.py
+++ test/namenormalizetest.py
@@ -1,35 +1,44 @@
 # This Python file uses the following encoding: utf-8
 ## begin license ##
 #
-#    Meresco Normalize is an open-source library containing normalization 
-#    components for use with Meresco
+# Meresco Normalize is an open-source library containing normalization
+# components for use with Meresco
 #
-#    Copyright (C) 2008-2009 Universiteit van Tilburg http://www.uvt.nl
-#    Copyright (C) 2008-2009 Technische Universiteit Delft http://www.tudelft.nl
-#    Copyright (C) 2008-2010 Seek You Too (CQ2) http://www.cq2.nl
+# Copyright (C) 2008-2010 Seek You Too (CQ2) http://www.cq2.nl
+# Copyright (C) 2008-2009 Technische Universiteit Delft http://www.tudelft.nl
+# Copyright (C) 2008-2009 Universiteit van Tilburg http://www.uvt.nl
+# Copyright (C) 2019 Seecr (Seek You Too B.V.) https://seecr.nl
 #
-#    This file is part of Meresco Normalize.
+# This file is part of "Meresco Normalize"
 #
-#    Meresco Normalize is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the License, or
-#    (at your option) any later version.
+# "Meresco Normalize" is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
-#    Meresco Normalize is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+# "Meresco Normalize" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with Meresco Normalize; if not, write to the Free Software
-#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# You should have received a copy of the GNU General Public License
+# along with "Meresco Normalize"; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 ## end license ##
+
 from StringIO import StringIO
 
 from unittest import TestCase
 
-from meresco.normalize.name.namenormalize import lastnameAndInitials, lastnameAndFirstInitial, lastname, unparsable, firstLetter, _breakUp, _helsing_v, _helsing_a_v, _helsing_ab_van
+from meresco.normalize.name.namenormalize import lastnameAndInitials, lastnameAndFirstInitial, lastname, unparsable, firstLetter, _breakUp, _helsing_v, _helsing_a_v, _helsing_ab_van, givenname, familyname, _BreakUpResult
+
+def helsing_v((lastname, initials, prefix)):
+    return _helsing_v(_BreakUpResult(lastname, '', initials, prefix))
+def helsing_a_v((lastname, initials, prefix)):
+    return _helsing_a_v(_BreakUpResult(lastname, '', initials, prefix))
+def helsing_ab_van((lastname, initials, prefix)):
+    return _helsing_ab_van(_BreakUpResult(lastname, '', initials, prefix))
 
 class NameNormalizeTest(TestCase):
 
@@ -269,67 +278,67 @@ class NameNormalizeTest(TestCase):
 
     def test_breakUp(self):
         self.assertEquals(None, _breakUp(""))
-        self.assertEquals([str, str, str], [type(e) for e in _breakUp("Peters, H.")])
-        self.assertEquals(("peters", "h.", ""), _breakUp("Peters, H."))
-        self.assertEquals(("peters", "h.j.m.", ""), _breakUp("Peters, H.J.M."))
-        self.assertEquals(("peters", "h.j.m.", ""), _breakUp("Peters, H. J. M."))
-        self.assertEquals(("guillen scholten", "j.v.", ""), _breakUp("Guillen Scholten, J. V."))
+        self.assertEquals([str, str, str, str], [type(e) for e in _breakUp("Peters, H.")])
+        self.assertEquals(("peters", "h.", "h.", ""), _breakUp("Peters, H."))
+        self.assertEquals(("peters", "h.j.m.", "h.j.m.", ""), _breakUp("Peters, H.J.M."))
+        self.assertEquals(("peters", "h. j. m.", "h.j.m.", ""), _breakUp("Peters, H. J. M."))
+        self.assertEquals(("guillen scholten", "j. v.", "j.v.", ""), _breakUp("Guillen Scholten, J. V."))
 
     def test_breakUpInitials(self):
-        self.assertEquals(("peters", "h.", ""), _breakUp("Peters, Hans"))
-        self.assertEquals(("peters", "h.", ""), _breakUp("Peters, H"))
-        self.assertEquals(("peters", "h.", ""), _breakUp("Peters, H. (Hans)"))
-        self.assertEquals(("peters", "h.", ""), _breakUp("Peters, H. (ed.)"))
-        self.assertEquals(("peters", "h.", ""), _breakUp("Peters. H."))
-        self.assertEquals(("peters", "h.j.m.", ""), _breakUp("Peters, H.J.M. (Hans)"))
-        self.assertEquals(("peters", "h.", ""), _breakUp("Peters, H. (Hans Jan Marie)"))
-        self.assertEquals(("peters", "h.j.m.", ""), _breakUp("Peters, HJM"))
-        self.assertEquals(("peters", "h.j.m.", ""), _breakUp("Peters, Hans Jan Marie"))
-        self.assertEquals(("peters", "j.j.", ""), _breakUp("Peters, J.-J."))
+        self.assertEquals(("peters", "hans", "h.", ""), _breakUp("Peters, Hans"))
+        self.assertEquals(("peters", "h", "h.", ""), _breakUp("Peters, H"))
+        self.assertEquals(("peters", "h.", "h.", ""), _breakUp("Peters, H. (Hans)"))
+        self.assertEquals(("peters", "h.", "h.", ""), _breakUp("Peters, H. (ed.)"))
+        self.assertEquals(("peters", "h.", "h.", ""), _breakUp("Peters. H."))
+        self.assertEquals(("peters", "h.j.m.", "h.j.m.", ""), _breakUp("Peters, H.J.M. (Hans)"))
+        self.assertEquals(("peters", "h.", "h.", ""), _breakUp("Peters, H. (Hans Jan Marie)"))
+        self.assertEquals(("peters", "hjm", "h.j.m.", ""), _breakUp("Peters, HJM"))
+        self.assertEquals(("peters", "hans jan marie", "h.j.m.", ""), _breakUp("Peters, Hans Jan Marie"))
+        self.assertEquals(("peters", "j.-j.", "j.j.", ""), _breakUp("Peters, J.-J."))
 
-        self.assertEquals(("peters", "j.j.", ""), _breakUp("Peters, J-J."))
-        self.assertEquals(("somers","v.a.m.c.",""), _breakUp("Somers, Veerle Anne-Marie Christine"))
-        self.assertEquals(("vissers","f.h.j.a.",""), _breakUp("Vissers, Frans H.J.A."))
-        self.assertEquals(("berg","g.j.","van den"), _breakUp("Berg, G. J. van den"))
-        self.assertEquals(("berg","g.j.","van den"), _breakUp("Berg, Gerard J. van den"))
-        self.assertEquals(("berg","a.p.","van den"), _breakUp("Berg, A.P. (Arie) van den"))
-        self.assertEquals(("franses","ph.h.b.f.",""), _breakUp("Franses, Ph.H.B.F.")) #??
-        self.assertEquals(("heiden","a.f.th.","van der"), _breakUp("Heiden, A.F.Th. van der")) #??
-        self.assertEquals(("peters", "h.", ""), _breakUp("Peters, Hans"))
-        self.assertEquals(("beaumont", "w.w.", ""), _breakUp("Beaumont, W.Worby"))
-        self.assertEquals(("petri", "n.", ""), _breakUp("Petri, Nicolaus (Daventriensis)"))
+        self.assertEquals(("peters", "j-j.", "j.j.", ""), _breakUp("Peters, J-J."))
+        self.assertEquals(("somers","veerle anne-marie christine","v.a.m.c.",""), _breakUp("Somers, Veerle Anne-Marie Christine"))
+        self.assertEquals(("vissers","frans h.j.a.","f.h.j.a.",""), _breakUp("Vissers, Frans H.J.A."))
+        self.assertEquals(("berg","g. j.","g.j.","van den"), _breakUp("Berg, G. J. van den"))
+        self.assertEquals(("berg","gerard j.","g.j.","van den"), _breakUp("Berg, Gerard J. van den"))
+        self.assertEquals(("berg","a.p.","a.p.","van den"), _breakUp("Berg, A.P. (Arie) van den"))
+        self.assertEquals(("franses","ph.h.b.f.","ph.h.b.f.",""), _breakUp("Franses, Ph.H.B.F.")) #??
+        self.assertEquals(("heiden","a.f.th.","a.f.th.","van der"), _breakUp("Heiden, A.F.Th. van der")) #??
+        self.assertEquals(("peters", "hans", "h.", ""), _breakUp("Peters, Hans"))
+        self.assertEquals(("beaumont", "w.worby", "w.w.", ""), _breakUp("Beaumont, W.Worby"))
+        self.assertEquals(("petri", "nicolaus", "n.", ""), _breakUp("Petri, Nicolaus (Daventriensis)"))
 
     def test_breakUpUtf8(self):
-        self.assertEquals(("lämmel", "r.", ""), _breakUp("Lämmel, R."))
-        self.assertEquals(("ämmel", "r.", ""), _breakUp("Ämmel, R."))
-        self.assertEquals(("lämmel", "é.", ""), _breakUp("Lämmel, É."))
-        self.assertEquals(("lämmel", "é.", ""), _breakUp("Lämmel, Érik"))
-        self.assertEquals(("lämmel", "é.", ""), _breakUp("Lämmel, É. (Érik)"))
-        self.assertEquals(("núñez-queija", "r.", ""), _breakUp('Núñez-Queija, R.'))
-        self.assertEquals(("açıkel","y.s.", ""), _breakUp("Açıkel, Y. Sağ"))
+        self.assertEquals(("lämmel", "r.", "r.", ""), _breakUp("Lämmel, R."))
+        self.assertEquals(("ämmel", "r.", "r.", ""), _breakUp("Ämmel, R."))
+        self.assertEquals(("lämmel", "é.", "é.", ""), _breakUp("Lämmel, É."))
+        self.assertEquals(("lämmel", "érik", "é.", ""), _breakUp("Lämmel, Érik"))
+        self.assertEquals(("lämmel", "é.", "é.", ""), _breakUp("Lämmel, É. (Érik)"))
+        self.assertEquals(("núñez-queija", "r.", "r.", ""), _breakUp('Núñez-Queija, R.'))
+        self.assertEquals(("açıkel","y. sağ","y.s.", ""), _breakUp("Açıkel, Y. Sağ"))
 
     def test_breakUpPrefix(self):
-        self.assertEquals(("eind", "a.", "'t"), _breakUp("Eind, A. 't"))
-        self.assertEquals(("peters", "h.", "van"), _breakUp("Peters, H. van"))
-        self.assertEquals(("peters", "h.", "van der"), _breakUp("Peters, H. van der"))
-        self.assertEquals(("peters", "h.j.m.", "van"), _breakUp("Peters, H.J.M. van"))
-        self.assertEquals(("peters", "h.j.m.", "van"), _breakUp("Peters, H. J. M. van"))
+        self.assertEquals(("eind", "a.", "a.", "'t"), _breakUp("Eind, A. 't"))
+        self.assertEquals(("peters", "h.", "h.", "van"), _breakUp("Peters, H. van"))
+        self.assertEquals(("peters", "h.", "h.", "van der"), _breakUp("Peters, H. van der"))
+        self.assertEquals(("peters", "h.j.m.", "h.j.m.", "van"), _breakUp("Peters, H.J.M. van"))
+        self.assertEquals(("peters", "h. j. m.", "h.j.m.", "van"), _breakUp("Peters, H. J. M. van"))
 
         #self.assertEquals(("peters", "h", "v"), _breakUp("Peters, H. v.")) #?
         #naam: achter tussen voor initialen titels
         #self.assertEquals(("peters", "", "j", ["j", "j"]), _breakUp("Peters, Jean-Jacques"))
 
     def testCompose(self):
-        self.assertEquals("Helsing", _helsing_v(("helsing", "a.b.", "")))
-        self.assertEquals("Helsing, v.", _helsing_v(("helsing", "a.b.", "van")))
-        self.assertEquals("Helsing, A.", _helsing_a_v(("helsing", "a.b.", "")))
-        self.assertEquals("Helsing, A. v.", _helsing_a_v(("helsing", "a.b.", "van")))
-        self.assertEquals("Helsing, A. v.d.", _helsing_a_v(("helsing", "a.b.", "van der")))
-        self.assertEquals("Helsing, A.B.", _helsing_ab_van(("helsing", "a.b.", "")))
-        self.assertEquals("Helsing, A.B. van", _helsing_ab_van(("helsing", "a.b.", "van")))
-        self.assertEquals("Helsing, A.B. van der", _helsing_ab_van(("helsing", "a.b.", "van der")))
-        self.assertEquals("IJmuiden", _helsing_v(("ijmuiden", "t.", "")))
-        self.assertEquals("Israël", _helsing_v(("israël", "t.", "")))
+        self.assertEquals("Helsing", helsing_v(("helsing", "a.b.", "")))
+        self.assertEquals("Helsing, v.", helsing_v(("helsing", "a.b.", "van")))
+        self.assertEquals("Helsing, A.", helsing_a_v(("helsing", "a.b.", "")))
+        self.assertEquals("Helsing, A. v.", helsing_a_v(("helsing", "a.b.", "van")))
+        self.assertEquals("Helsing, A. v.d.", helsing_a_v(("helsing", "a.b.", "van der")))
+        self.assertEquals("Helsing, A.B.", helsing_ab_van(("helsing", "a.b.", "")))
+        self.assertEquals("Helsing, A.B. van", helsing_ab_van(("helsing", "a.b.", "van")))
+        self.assertEquals("Helsing, A.B. van der", helsing_ab_van(("helsing", "a.b.", "van der")))
+        self.assertEquals("IJmuiden", helsing_v(("ijmuiden", "t.", "")))
+        self.assertEquals("Israël", helsing_v(("israël", "t.", "")))
 
     def testFirstLetter(self):
         self.assertEquals(int('f1', 16), ord(u'ñ'))
@@ -340,11 +349,11 @@ class NameNormalizeTest(TestCase):
         self.assertEquals([str('Ñ')], list(firstLetter("ñiño, el")))
 
     def testNamesInOtherFormat(self):
-        self.assertEquals(('groeneveld', 'e.', ''), _breakUp('E. Groeneveld'))
-        self.assertEquals(('groeneveld', 'e.g.', ''), _breakUp('E. G. Groeneveld'))
-        self.assertEquals(('groeneveld', 'é.û.', ''), _breakUp('É. Û. Groeneveld'))
+        self.assertEquals(('groeneveld', 'e.', 'e.', ''), _breakUp('E. Groeneveld'))
+        self.assertEquals(('groeneveld', 'e. g.', 'e.g.', ''), _breakUp('E. G. Groeneveld'))
+        self.assertEquals(('groeneveld', 'é. û.', 'é.û.', ''), _breakUp('É. Û. Groeneveld'))
         brokenUp = _breakUp('H. G. Ruhé')
-        self.assertEquals(('ruhé', 'h.g.', ''), brokenUp)
+        self.assertEquals(('ruhé', 'h. g.', 'h.g.', ''), brokenUp)
         result = _helsing_a_v(brokenUp)
         self.assertEquals('Ruhé, H.', result)
 
@@ -361,3 +370,65 @@ class NameNormalizeTest(TestCase):
         #self.assertLastnameAndInitials('Agricola, G.', "Agricola, Georgius.")
         #self.assertLastnameAndInitials('?', "Ahiaku, geb. Brew, Comfort Akyima")
         #self.assertLastnameAndInitials('Ahlburg, J.', "Ahlburg, Joh.")
+
+    def testFamilynameGivenname(self):
+        def assertFamilynameGivenname(expected_familyname, expected_givenname, name):
+            familyresult = first(familyname(name))
+            givenresult = first(givenname(name))
+            self.assertEqual((expected_familyname, expected_givenname), (familyresult, givenresult))
+        assertFamilynameGivenname('Peters', 'H.', "Peters, H.")
+        # ("Peters, H.J.M.")
+        # ("Peters, H.J.M.", "Peters, H. J. M.")
+        # ("Guillen Scholten, J.V.", "Guillen Scholten, J. V.")
+        # ("Peters, H.", "Peters, Hans")
+        # ("Peters, H.", "Peters, H")
+        # ("Peters, H.", "Peters, H. (Hans)")
+        # ("Peters, H.J.M.", "Peters, H.J.M. (Hans)")
+        # ("Peters, H.", "Peters, H. (Hans Jan Marie)")
+        # ("Peters, H.J.M.", "Peters, HJM")
+        # ("Peters, H.J.M.", "Peters, Hans Jan Marie")
+        # ("Peters, J.J.", "Peters, J.-J.")
+        # ("Peters, J.J.", "Peters, J-J.")
+        # ("Somers, V.A.M.C.", "Somers, Veerle Anne-Marie Christine")
+        # ("Vissers, F.H.J.A.", "Vissers, Frans H.J.A.")
+        # ("Berg, G.J. van den", "Berg, G. J. van den")
+        # ("Berg, G.J. van den", "Berg, Gerard J. van den")
+        # ("Franses, Ph.H.B.F.")
+        # ("Heiden, A.F.Th. van der")
+        # ("Lämmel, R.")
+        # ("Ämmel, R.")
+        # ("Ba'ath, R.")
+        # ("IJssel, R.")
+        # ("Lämmel, É.")
+        # ("Lämmel, É.", "Lämmel, Érik")
+        # ("Lämmel, É.", "Lämmel, É. (Érik)")
+        # ('Núñez-Queija, R.')
+        # ("Eind, A. 't")
+        # ("Peters, H. van")
+        # ("Peters, H. van der")
+        # ("Peters, H.J.M. van")
+        # ("Peters, H.J.M. van", "Peters, H. J. M. van")
+        # ("Bart, P.A.", "Bart, P.-A. (Pierre-Alexandre)")
+        #self.assertLastnameAndInitials("Groeneveld, E.J.", "Erik J.Groeneveld")
+        #self.assertLastnameAndInitials("Bunch, T.J.", "Bunch.T.Jared")
+        #self.assertLastnameAndInitials("Bunch, T.J.", "B T.Bunch")
+        #self.assertLastnameAndInitials("Bunch, T.J.", "B.T.Bunch")
+        #self.assertLastnameAndInitials("Jared-Morehouse, B.T.", "Bunch T.Jared-Morehouse")
+        # ("Ba'ath, B.T.", "Bunch T.Ba'ath")
+        # ("Groeneveld, E.J.", "E.J. Groeneveld (Lord)")
+        # ("Groeneveld, E.", "E. (Groeneveld)")
+        # ("Groeneveld", "Groeneveld")
+        # ("Groeneveld", "(Groeneveld)")
+        # ("Groeneveld-Dijkema", "(Groeneveld-Dijkema)")
+        # ("Groen'veld", "(Groen'veld)")
+        # ("Groen'veld-Dijkema", "(Groen'veld-Dijkema?)")
+        # ("Açıkel, Y.S.", "Açıkel, Y. Sağ")
+
+
+def first(it, default=None):
+    try:
+        return it.next()
+    except StopIteration:
+        return default
+
+
